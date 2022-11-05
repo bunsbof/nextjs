7c68f5dce102a73f1d2d9f015d334e2ec39fbcad
The <nguyenminhcuong2000@gmail.com>
2022-11-05 22:09:30 +0700

    #2022/11/05 cuong - Initial nextjs test project

---
+++ test/app/todos/TodosList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Todo } from '../../typings'
+
+const fetchTodos = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const todos : Todo[] = await res.json
+  return todos;
+}
+
+async function TodosList() {
+  const todos = await fetchTodos()
+  return (
+    <div>TodosList</div>
+  )
+}
+
+export default TodosList
